7cffff69da531e7aecd621bc9170aa0ecd058f4e
Eldin Kecap <eldinkecap@gmail.com>
2022-10-28 00:19:46 +0200

    worked on css and fixed the channnel search by using search api instead of channel api

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ async function search(){
     errorDiv.innerHTML = '';
   }
   if(userInputValue != ''){
-    let url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&forUsername=${userInputValue}&key=AIzaSyBDfzpEUdss3yp6v9ml5KDuBG7FzUT8HAA`;
+    userInputValue.trim().replace(' ','%20');
+    let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${userInputValue}&key=AIzaSyBDfzpEUdss3yp6v9ml5KDuBG7FzUT8HAA`;
     let response = await fetch(url);
     data = await response.json();
     results.innerHTML='';  
@@ -18,68 +19,77 @@ async function search(){
     userInput.classList.add('red-placeholder-text')
     return
   }
+  let flagForNoChannels = true;
   if (Object.keys(data).includes('items')) {
     for(item of data.items){
-      results.appendChild(generateChannelCard(item));
+      if (item.id.kind === 'youtube#channel') { 
+        flagForNoChannels = false;
+        results.appendChild(generateChannelCard(item));
+      }
     }
   } else {
-    errorDiv.innerHTML = 'No search results found';
-    userInput.value = '';
+    errorDiv.innerHTML = 'No channels results found';
+    // userInput.value = '';
+  }
+  if (flagForNoChannels) {
+    errorDiv.innerHTML = 'No channels results found';  
   }
 }
 
 function generateChannelCard(item) {
-    let link = document.createElement('a');
-    let container = document.createElement('div');
-    let image = document.createElement('img');
-    let title = document.createElement('h3');
-    let description = document.createElement('p');
-    let statistics = createStatisticsElement(item);
-    let showVideosButton = document.createElement('a');
-    link.setAttribute('href','https://youtube.com/channel/' + item.id);
-    link.setAttribute('target','_blank');
-    container.id = 'channelContainer';
-    container.className = 'container';
-    container.classList.add('blue-grey','darken-4','grey-text','text-lighten-3','waves-effect','waves-light');
-    description.classList.add('flow-text');
-    title.innerHTML = item.snippet.title;
-    description.innerHTML = item.snippet.description;
-    image.src = item.snippet.thumbnails.default.url;
-    image.width = item.snippet.thumbnails.default.width;
-    image.height = item.snippet.thumbnails.default.height;
-    image.classList.add('circle','responsive-img');
-    link.innerHTML = 'Go to Channel';
-    link.classList.add('btn-small');
-    showVideosButton.innerHTML = 'Show videos';
-    showVideosButton.setAttribute('href','#videoResults');
-    showVideosButton.classList.add('modal-trigger','btn-small','blue','lighten-2');
-    showVideosButton.addEventListener('click',()=>{
-      showVideosButtonEvent(item);
-    },{once:true})
-    container.appendChild(image);
-    container.appendChild(statistics);
-    container.appendChild(title);
-    container.appendChild(link);
-    container.appendChild(showVideosButton);
-    container.appendChild(description);
-    return container;
-}
+      let link = document.createElement('a');
+      let container = document.createElement('div');
+      let image = document.createElement('img');
+      let title = document.createElement('h3');
+      let description = document.createElement('p');
+      // let statistics = createStatisticsElement(item);
+      let showVideosButton = document.createElement('a');
+      link.setAttribute('href','https://youtube.com/channel/' + item.id);
+      link.setAttribute('target','_blank');
+      container.id = 'channelContainer';
+      container.className = 'container';
+      container.classList.add('grey','lighten-4','black-text','waves-effect','waves-light');
+      description.classList.add('flow-text');
+      title.innerHTML = item.snippet.title;
+      description.innerHTML = item.snippet.description;
+      image.src = item.snippet.thumbnails.default.url;
+      // image.width = item.snippet.thumbnails.default.width;
+      // image.height = item.snippet.thumbnails.default.height;
+      image.classList.add('circle','responsive-img');
+      link.innerHTML = 'Go to Channel';
+      link.classList.add('btn-small');
+      showVideosButton.innerHTML = 'Show videos';
+      showVideosButton.setAttribute('href','#videoResults');
+      showVideosButton.classList.add('modal-trigger','btn-small','blue','lighten-2');
+      showVideosButton.addEventListener('click',()=>{
+        let videoResults = document.querySelector('.modal-content');
+        videoResults.innerHTML = '<h1>Video Results</h1>';
+        showVideosButtonEvent(item);
+      });
+      container.appendChild(image);
+      // container.appendChild(statistics);
+      container.appendChild(title);
+      container.appendChild(link);
+      container.appendChild(showVideosButton);
+      container.appendChild(description);
+      return container;
+  }
 
-function createStatisticsElement(item){
-    let statistics = document.createElement('div');
-    let viewCount = document.createElement('p');
-    let subscriberCount = document.createElement('p');
-    let videoCount = document.createElement('p');
-    statistics.classList.add('right');
-    statistics.id = 'statistics';
-    viewCount.innerHTML = `${item.statistics.viewCount} Views`;
-    subscriberCount.innerHTML = `${item.statistics.subscriberCount} Subscribers`;
-    videoCount.innerHTML = `${item.statistics.videoCount} Videos`;
-    statistics.appendChild(viewCount);
-    statistics.appendChild(subscriberCount);
-    statistics.appendChild(videoCount);
-    return statistics
-}
+// function createStatisticsElement(item){
+//     let statistics = document.createElement('div');
+//     let viewCount = document.createElement('p');
+//     let subscriberCount = document.createElement('p');
+//     let videoCount = document.createElement('p');
+//     statistics.classList.add('right');
+//     statistics.id = 'statistics';
+//     viewCount.innerHTML = `${item.statistics.viewCount} Views`;
+//     subscriberCount.innerHTML = `${item.statistics.subscriberCount} Subscribers`;
+//     videoCount.innerHTML = `${item.statistics.videoCount} Videos`;
+//     statistics.appendChild(viewCount);
+//     statistics.appendChild(subscriberCount);
+//     statistics.appendChild(videoCount);
+//     return statistics
+// }
 
 async function fetchVideos(id,nextPage = 0){
     let response;
@@ -95,14 +105,18 @@ async function fetchVideos(id,nextPage = 0){
 }
 
 async function showVideosButtonEvent(item){
-    let videoData = await fetchVideos(item.id);
+    let videoData = await fetchVideos(item.id.channelId);
     let container = document.createElement('div');
-    container.classList.add('row');
-    for (let video of videoData.items ) {        
-      let card = generateVideoCard(video);
-      container.appendChild(card);
-    }
     let videoResults = document.querySelector('.modal-content');
+    // videoResults.innerHTML = '<h1>Video Results</h1>';
+    container.classList.add('row');
+    // console.log(videoData);
+    for (let video of videoData.items ) {  
+      if (video.id.kind === 'youtube#video') {
+        let card = generateVideoCard(video);
+        container.appendChild(card);
+      }      
+    }
     videoResults.appendChild(container);
 }
 
@@ -125,7 +139,7 @@ function generateVideoCard(video) {
   title.classList.add('flow-text','card-title');
   description.innerHTML = video.snippet.description;
   description.classList.add('flow-text');
-  publishTime.innerHTML = video.snippet.publishTime;
+  publishTime.innerHTML = `Upload time: ${video.snippet.publishTime.replace('T',' ').replace('Z','')}`;
   publishTime.classList.add('flow-text');
   image.setAttribute('src',video.snippet.thumbnails.high.url);
   image.width = video.snippet.thumbnails.high.width ;
